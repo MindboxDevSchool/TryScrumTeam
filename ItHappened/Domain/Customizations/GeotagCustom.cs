50d9e8e275b59af7c31d29a7a8706c535a59bd39
Ilya <ilyaaxenov1999@gmail.com>
2020-10-17 14:18:39 +0300

    Customs and custom types added

---
+++ ItHappened/Domain/Customizations/GeotagCustom.cs
@@ -0,0 +1,15 @@
+﻿namespace ItHappened.Domain.Customizations
+{
+    public class GeotagCustom
+    {
+        public GeotagCustom(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double Latitude { get; }
+
+        public double Longitude { get; }
+    }
+}
--- ItHappened/Domain/Customs.cs
+++ ItHappened/Domain/Customs.cs
@@ -1,7 +1,27 @@
-﻿namespace ItHappened.Domain
+﻿using System;
+using ItHappened.Domain.Customizations;
+
+namespace ItHappened.Domain
 {
     public class Customs
     {
-        
+        public Customs(string comment, RatingCustom rating, int scale, byte[] photo, GeotagCustom geotag)
+        {
+            Comment = comment ?? throw new ArgumentNullException(nameof(comment));
+            Rating = rating ?? throw new ArgumentNullException(nameof(rating));
+            Scale = scale;
+            Photo = photo ?? throw new ArgumentNullException(nameof(photo));
+            Geotag = geotag ?? throw new ArgumentNullException(nameof(geotag));
+        }
+
+        public string Comment { get; }
+
+        public RatingCustom Rating { get; }
+
+        public int Scale { get; }
+
+        public byte[] Photo { get; }
+
+        public GeotagCustom Geotag { get; }
     }
 }
