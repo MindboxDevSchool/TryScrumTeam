4fa6ce031f1c506326988fb3622665402ea1a374
AndrewGronin <you@example.com>
2020-10-23 16:36:05 +0300

    Fixed unspec exception

--- ItHappened/Infrastructure/Repositories/UserRepositoryInMemory.cs
+++ ItHappened/Infrastructure/Repositories/UserRepositoryInMemory.cs
@@ -24,7 +24,7 @@ namespace ItHappened.Infrastructure.Repositories
             var result = _users.FirstOrDefault(elem => elem.Value.Login == login);
             
             if (result.Equals(default(KeyValuePair<Guid, User>)))
-                throw new RepositoryException(RepositoryExceptionType.UserNotFound, login);
+                throw new RepositoryException(RepositoryExceptionType.UserNotFoundByLogin, login);
             
             return result.Value;
         }
