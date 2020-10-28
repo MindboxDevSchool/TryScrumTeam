c6f6192ae8b23f422740cd04d102c00dbfc41846
AndrewGronin <you@example.com>
2020-10-28 19:44:38 +0300

    Working on efTrackRepo

---
+++ ItHappened/Infrastructure/Repositories/TrackRepositoryEFDataBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ItHappened.Domain;
+using ItHappened.Domain.Repositories;
+
+namespace ItHappened.Infrastructure.Repositories
+{
+    public class TrackRepositoryEfDataBase : ITrackRepository
+    {
+        private TrackDbContext _trackDbContext;
+        
+        public TrackRepositoryEfDataBase(TrackDbContext trackDbContext)
+        {
+            _trackDbContext = trackDbContext;
+        }
+
+        public Track TryCreate(Track track)
+        {
+            _trackDbContext.Tracks.Add(track);
+            return track;
+        }
+
+        public IEnumerable<Track> TryGetTracksByUser(Guid userId)
+        {
+            return 
+                _trackDbContext
+                    .Tracks
+                    .Where(track => track.CreatorId == userId);
+        }
+
+        public Track TryGetTrackById(Guid trackId)
+        {
+            return
+                _trackDbContext
+                    .Tracks
+                    .Single(track => track.Id == trackId);
+        }
+
+        public Track TryUpdate(Track track)
+        {
+            return track;
+        }
+
+        public Guid TryDelete(Guid trackId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ItHappened/Infrastructure/TrackDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using ItHappened.Domain;
+
+namespace ItHappened.Infrastructure
+{
+    public class TrackDbContext : DbContext
+    {
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
