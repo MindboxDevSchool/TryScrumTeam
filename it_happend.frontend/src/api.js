9927510bafd67534f617e70cbed3e10848fac4a8
moskovets <n.moskovets@yandex.ru>
2020-11-07 21:11:05 +0300

    fix auth and 404

--- it_happend.frontend/src/api.js
+++ it_happend.frontend/src/api.js
@@ -6,10 +6,21 @@ const instance = axios.create({
     baseURL: apiUrl,
 })
 
+function errorForLogingHandler(error) {
+    if (error.response) {
+        if (error.response.statis >= 500)
+            window.alert("Что-то пошло не так, пожалуйста обновите страницу или попробуйте позже!");
+    }
+    else {
+        window.alert("Что-то пошло не так, пожалуйста обновите страницу или попробуйте позже!");
+    }
+}
+
 function errorHandler(error) {
     if (error.response) {
         if (error.response.status === 404) {
             history.push('/404');
+            window.location.reload(false);
         } 
         if (error.response.status === 401) {
             localStorage.removeItem('token');
@@ -33,8 +44,8 @@ function authHeader() {
 
 
 //users
-export const createUser = (login, password) => instance.post(`/user`, { login: login, password: password }).then(result => result.data).catch(errorHandler);
-export const loginUser = (login, password) => instance.post(`/authentication`, { login: login, password: password }).then(result => result.data).catch(errorHandler);
+export const createUser = (login, password) => instance.post(`/user`, { login: login, password: password }).then(result => result.data).catch(errorForLogingHandler);
+export const loginUser = (login, password) => instance.post(`/authentication`, { login: login, password: password }).then(result => result.data).catch(errorForLogingHandler);
 
 
 //Events
