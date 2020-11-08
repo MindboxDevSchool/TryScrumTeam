9a0b3185ab56ba4816d6f14ddcc0749e95bf0bd3
moskovets <n.moskovets@yandex.ru>
2020-11-08 14:27:50 +0300

    fix event box styles

--- it_happend.frontend/src/Components/Events/EventBox.js
+++ it_happend.frontend/src/Components/Events/EventBox.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardActions, CardContent, Typography, Button } from "@material-ui/core";
+import { Card, CardActions, CardContent, Typography, Divider, IconButton } from "@material-ui/core";
+import { Edit, Delete, Comment, Room } from '@material-ui/icons';
 import Rating from '@material-ui/lab/Rating';
 import { Link } from "react-router-dom"
 import { deleteEvent } from './../../api';
+import { grey } from '@material-ui/core/colors';
+
 import moment from 'moment'
 import 'moment/locale/ru'
 moment.locale('ru')
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 275,
+    width: 390,
     marginBottom: theme.spacing(2),
+    marginRight: theme.spacing(2),
     borderRadius: 10
   },
   bullet: {
@@ -20,73 +24,102 @@ const useStyles = makeStyles((theme) => ({
     transform: "scale(0.8)"
   },
   title: {
-    fontSize: 14
+    fontSize: 14,
+    marginRight: theme.spacing(1)
   },
   pos: {
     marginBottom: 12
   },
   geoTag: {
     marginRight: 10
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  ratingContainer: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  photoContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: 200,
+  },
+
+  geotagContainer: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  commentContainer: {
+    backgroundColor: grey[200],
+    padding: theme.spacing(2),
+    borderRadius: 10,
+    position: 'relative',
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
   }
+
 }));
 export default function EventBox(props) {
   const classes = useStyles();
-  const comment = <div>
-    <Typography
-      className={classes.title}
-      color="textSecondary"
-      gutterBottom
-    >
-      Комментарий
-                    </Typography>
-    <Typography variant="h5" component="h2">
-      {props.comment}
-    </Typography>
-  </div>;
+  const comment =
+    <div className={classes.commentContainer}>
+      <Comment />
+      <Typography variant="h6" component="h2">
+        {props.comment}
+      </Typography>
+    </div>;
 
-  const rating = <div>
-    <Typography
-      className={classes.title}
-      color="textSecondary"
-      gutterBottom
-    >
-      Оценка
+  const rating =
+    <div className={classes.ratingContainer}>
+      <Typography className={classes.title} color="textSecondary">
+        Оценка:
+      </Typography>
+      <Rating max={10} value={props.rating} readOnly />
+    </div>;
+
+  function openGoogleMapInNewTab() {
+    const url = `https://www.google.com/maps/place/${props.geotagLatitude}+${props.geotagLongitude}`;
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  const geoTag =
+    <div className={classes.geotagContainer}>
+      <Typography
+        className={classes.title}
+        color="textSecondary"
+      >
+        Местоположение:
         </Typography>
-    <Rating max={10} value={props.rating} readOnly />
-  </div>;
+      <IconButton
+        onClick={openGoogleMapInNewTab}>
+        <Room />
+      </IconButton>
+    </div>;
 
-  const geoTag = <div>
-    <Typography
-      className={classes.title}
-      color="textSecondary"
-      gutterBottom
-    >
-      Местоположение
-        </Typography>
-    <Typography className={classes.geoTag} display="inline" variant="h5" component="h2">
-      Широта  {props.geotagLatitude}
-    </Typography>
-    <Typography display="inline" variant="h5" component="h2">
-      Долгота {props.geotagLongitude}
-    </Typography>
-  </div>;
+  const scale =
+    <div className={classes.ratingContainer}>
+      <Typography
+        className={classes.title}
+        color="textSecondary"
+      >
+        Шкала:
+      </Typography>
+      <Typography variant="h5" component="h2">
+        {props.scale}
+      </Typography>
+    </div>;
 
-  const scale = <div>
-    <Typography
-      className={classes.title}
-      color="textSecondary"
-      gutterBottom
-    >
-      Шкала
-        </Typography>
-    <Typography variant="h5" component="h2">
-      {props.scale}
-    </Typography>
-  </div>;
-
-  const photo = <div>
-    <img src={props.photoUrl} width="50%" />
-  </div>;
+  const photo =
+    <div className={classes.photoContainer}>
+      <img src={props.photoUrl} alt="" width="auto" height="100%" />
+    </div>;
 
   const onRouteToEdit = () => {
     localStorage.setItem('event', JSON.stringify(props))
@@ -115,48 +148,36 @@ export default function EventBox(props) {
         <Card className={classes.root}>
           <CardContent >
             <Typography
-              className={classes.title}
-              color="textSecondary"
+              variant='h6'
+              color="textPrimary"
               gutterBottom
             >
               {moment(props.createdAt).format('LL h:mm a')}
             </Typography>
-            {props.comment != null
-              ?
-              comment
-              :
-              <div />
-            }
-            {props.rating != null
-              ?
-              rating
-              :
-              <div />
-            }
-            {props.scale != null
-              ?
-              scale
-              :
-              <div />
-            }
-            {props.geotagLatitude != null && props.geotagLongitude != null
-              ?
-              geoTag
-              :
-              <div />
-            }
-            {props.photoUrl != null
-              ?
-              photo
-              :
-              <div />
+            <Divider />
+            {props.rating != null ? rating : null}
+            {props.comment != null ? comment : null}
+            {props.scale != null ? scale : null}
+            {props.geotagLatitude != null && props.geotagLongitude != null ? geoTag : null}
+            {props.photoUrl != null ? photo : null}
+
+            {props.rating != null || props.comment != null || props.scale != null || props.geotagLatitude != null || props.geotagLongitude != null || props.photoUrl != null
+              ? <Divider />
+              : null
             }
           </CardContent>
-          <CardActions>
-            <Button disable={isDeleting} size="small" onClick={DeleteEvent}>Удалить</Button>
-            <Link to={`/tracks/${props.trackId}/events/${props.id}/edit`} style={{ textDecoration: 'none' }} onClick={onRouteToEdit}>
-              <Button size="small">Изменить</Button>
+          <CardActions className={classes.cardActions}>
+            <Link to={`/tracks/${props.trackId}/events/${props.id}/edit`} onClick={onRouteToEdit}>
+              <IconButton aria-label="edit">
+                <Edit fontSize="small" />
+              </IconButton>
             </Link>
+            <IconButton
+              aria-label="delete"
+              onClick={DeleteEvent}
+              disabled={isDeleting}>
+              <Delete fontSize="small" />
+            </IconButton>
           </CardActions>
         </Card>
         :
